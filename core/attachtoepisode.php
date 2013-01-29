7bce6c574fcb9c058fbb6a26e6ab2a7dd81e295e
Alberto B. <albertobeta@users.noreply.github.com>
2013-01-29 12:04:05 +0000

    new (advance) option. If a file called embed-code.txt is manually created in the root of Podcast Generator. The content of that file will be displayed along with each episode (useful to add customized HTML code to each episode)

--- core/attachtoepisode.php
+++ core/attachtoepisode.php
@@ -4,8 +4,12 @@
 //Define the full URL of a single episode
 	$fullURL = $url.'?p=episode&amp;name='.$file_multimediale[0].'.'.$podcast_filetype; //full URL of the episode
 
-
 	
+// CUSTOMIZED CODE TO EMBED
+// IF a file called embed-code.txt is manually created in the root of Podcast Generator. The content of that file will be displayed along with each episode (useful to add customized HTML code to each episode)
+	if(file_exists("$absoluteurl"."embed-code.txt")){
+		$embeddedcodetoshow = file_get_contents("$absoluteurl"."embed-code.txt");
+		$PG_mainbody .= $embeddedcodetoshow; }
 	
 	
 	//SOCIAL NETWORKS INTEGRATION
