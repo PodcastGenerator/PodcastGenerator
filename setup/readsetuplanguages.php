5c18c811b5c2a91c07af5a351dc5b1ec39cbb16f
Alberto B. <albertobeta@users.noreply.github.com>
2013-04-11 15:57:38 +0000

    fixed xml parser in setup

--- setup/readsetuplanguages.php
+++ setup/readsetuplanguages.php
@@ -14,15 +14,20 @@ if (isset($_REQUEST['GLOBALS']) OR isset($_REQUEST['absoluteurl']) OR isset($_RE
 
 //Get the XML document loaded into a variable (The xml parser must be previously included)
 
-if (file_exists("$absoluteurl"."components/podcastgen_languages/podcastgen_languages.xml")) {
+if (file_exists($absoluteurl."components/podcastgen_languages/podcastgen_languages.xml")) {
 
+/*
 	$xml = file_get_contents("$absoluteurl"."components/podcastgen_languages/podcastgen_languages.xml");
 	//Set up the parser object
 	$parser = new XMLParser($xml);
 
 	//Parse the XML file with categories data...
 	$parser->Parse();
-
+*/
+	
+	$parser = simplexml_load_file($absoluteurl.'components/podcastgen_languages/podcastgen_languages.xml','SimpleXMLElement',LIBXML_NOCDATA);
+	
+	
 }
 
 
