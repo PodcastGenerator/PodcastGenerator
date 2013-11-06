de4f4d490325752f8ec3185fa1e64ec2c4b3851f
Alberto B. <albertobeta@users.noreply.github.com>
2013-11-06 21:52:55 +0000

    Fixed Submit link to iTunes Store

--- core/admin/admin.php
+++ core/admin/admin.php
@@ -162,7 +162,7 @@ if (isset($_GET['p'])) if ($_GET['p']=="admin") { // if admin is called from the
 			<ul> 
 			<li><a href="?p=admin&do=itunesimg">'._("Change iTunes Cover Art").'</a></li>
 			<li><a href="?p=admin&do=itunescat">'._("Select or change iTunes Categories").'</a></li>
-			<li><a href="https://phobos.apple.com/WebObjects/MZFinance.woa/wa/publishPodcast?feedURL='.$url.$feed_dir.'feed.xml" target="_blank">'._("Submit your podcast to the iTunes Store").'</a></li>
+			<li><a href="https://buy.itunes.apple.com/WebObjects/MZFinance.woa/wa/publishPodcast?feedURL='.$url.$feed_dir.'feed.xml" target="_blank">'._("Submit your podcast to the iTunes Store").'</a></li>
 		</ul>
 			</div>
 
