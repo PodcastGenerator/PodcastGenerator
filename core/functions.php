b300378b7e6626ddfb8a6edfd2d686b7cc8d1ba4
yellowjug <yellowjug@30dc1123-3d68-4ce6-b511-1f8584badd10>
2014-04-16 17:37:16 +0000

    fixed categories names in languages different encoding systems (e.g. Asian languages)

--- core/functions.php
+++ core/functions.php
@@ -1119,4 +1119,22 @@ function languagesList ($absoluteurl,$isTranslation) { // $isTranslation TRUE wh
 }
 
 
+function random_str($size) 
+{ 
+	$text = NULL;
+        $randoms = array( 
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", 
+                "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
+                "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" 
+        ); 
+
+        srand ((double) microtime() * 1000000); 
+
+        for($i = 1; $i <= $size; $i++) 
+            $text .= $randoms[(rand(0,35))]; 
+
+        return $text; 
+} 
+
+
 ?>
