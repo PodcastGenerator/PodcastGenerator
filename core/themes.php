d72eff6a55c41aa8b7a9765112ef78cebca2b357
Alberto B. <albertobeta@users.noreply.github.com>
2014-10-17 11:52:03 +0000

    moved the embedded CSS styles to common.css

--- core/themes.php
+++ core/themes.php
@@ -141,10 +141,12 @@ $theme_file_contents = str_replace("-----PG_JSLOAD-----", $loadjavascripts, $the
 
 
 //LOAD A CSS WITH CLASSES COMMON TO ALL THE THEMES
-$commonCSSurl = '<link href="themes/common.css" rel="stylesheet">';
-$commonCSSurl .= '<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">';
-$theme_file_contents = str_replace("-----PG_COMMONCSSLOAD-----", $commonCSSurl, $theme_file_contents); 
 
+// Commented common.css cause it creates artifacts with the old theme
+//$commonCSSurl = '<link href="themes/common.css" rel="stylesheet">';
+$commonCSSurl = '<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">';
+
+$theme_file_contents = str_replace("-----PG_COMMONCSSLOAD-----", $commonCSSurl, $theme_file_contents); 
 
 
 
