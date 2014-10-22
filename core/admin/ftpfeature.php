f48b3a6f8ec1d2ce9e21d5a0a670b649049fe355
Alberto B. <albertobeta@users.noreply.github.com>
2014-10-22 16:31:24 +0000

    FTP feature default name and description for episodes

--- core/admin/ftpfeature.php
+++ core/admin/ftpfeature.php
@@ -37,7 +37,7 @@ if (isset($_GET['p'])) if ($_GET['p']=="admin") { // if admin is called from the
 
 	} elseif (isset($_GET['c']) AND isset($_GET['p']) AND $_GET['p']=="admin" AND isset($_GET['do']) AND $_GET['do']=="ftpfeature") {
 
-		
+	
 	$episodesCounter = autoIndexingEpisodes();
 
 
