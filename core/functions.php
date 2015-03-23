1dc88fbc334fb087f57d71ef1dd88e1282df531d
yellowjug <yellowjug@30dc1123-3d68-4ce6-b511-1f8584badd10>
2015-03-23 14:39:27 +0000

    Solved presence of & in short description in feed RSS. Now converted

--- core/functions.php
+++ core/functions.php
@@ -111,7 +111,8 @@ function depurateContent($content) {
 	$content = stripslashes($content);
 	$content = str_replace('<', '&lt;', $content);
 	$content = str_replace('>', '&gt;', $content);
-	$content = str_replace('& ', '&amp; ', $content);
+	$content = str_replace('&amp;', '&', $content); //double subsitution to avoid &&&amps;
+	$content = str_replace('&', '&amp;', $content);
 	$content = str_replace('’', '&apos;', $content);
 	$content = str_replace('"', '&quot;', $content);
 	$content = str_replace('©', '&#xA9;', $content);
