db193a694c2558fb7aa5145d4c4f6195320594a9
Emil Engler <me@emilengler.com>
2019-10-27 20:01:59 +0100

    Make navbar modular

---
+++ themes/default/navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.php"><?php echo $config["podcast_title"]; ?></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="categories.php">Categories</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="admin/">Admin</a>
+            </li>
+        </ul>
+    </div>
+</nav>
