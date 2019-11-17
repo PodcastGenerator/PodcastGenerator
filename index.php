cb8bd00d1caae7929c016e06c187dc7f925846af
Emil Engler <me@emilengler.com>
2019-11-17 05:04:15 +0100

    Make max_recent functionable

--- index.php
+++ index.php
@@ -3,7 +3,12 @@ session_start();
 require "core/include.php";
 // Regenerate RSS feed on every page access
 generateRSS("./");
-$episodes = getEpisodes();
+$episodes = getEpisodes(null);
+
+if(strtolower($config["max_recent"]) != "all") {
+    $episodes = array_slice($episodes, 0, $config["max_recent"]);
+}
+
 $buttons = getButtons("./");
 require $config["theme_path"]."index.php";
 ?>
