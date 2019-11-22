bf362b8fb367a85bd4668bf5842b67ab81d9eb28
Emil Engler <me@emilengler.com>
2019-11-22 23:09:22 +0100

    fix in categories list episodes

--- categories.php
+++ categories.php
@@ -2,5 +2,11 @@
 require "core/include.php";
 $categories = simplexml_load_file("categories.xml");
 $episodes = getEpisodes($_GET["cat"]);
+
+// Backwards comp
+$link = str_replace("?", "", $config["link"]);
+$link = str_replace("=", "", $link);
+$link = str_replace("\$url", "", $link);
+
 require $config["theme_path"]."categories.php";
 ?>
