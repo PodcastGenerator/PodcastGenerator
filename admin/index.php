4f00ee4e5b5b5ee90423d4dad8a25cdb2ddd9215
Emil Engler <me@emilengler.com>
2019-11-22 21:46:14 +0100

    Add PG news

--- admin/index.php
+++ admin/index.php
@@ -9,6 +9,12 @@ require "../core/include_admin.php";
     <title><?php echo htmlspecialchars($config["podcast_title"]); ?> - Admin</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="../core/bootstrap/style.css">
+    <style>
+        iframe {
+            width: 1000px;
+            height: 500px;
+        }
+    </style>
 </head>
 
 <body>
@@ -19,6 +25,11 @@ require "../core/include_admin.php";
     <br>
     <div class="container">
         <h1>Welcome to your Podcast Generator Admin Interface</h1>
+        <?php
+        if ($config["enablepgnewsinadmin"] == "yes") {
+            echo '<iframe width="100%" src=' . $news_url . '></iframe>';
+        }
+        ?>
     </div>
 </body>
 
--- core/misc/globs.php
+++ core/misc/globs.php
@@ -1,2 +1,3 @@
 <?php
 $version = 3.0;
+$news_url = "https://www.emilengler.com/pg/news.html";
