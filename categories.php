e6177e42dd5f73d7853180bfaef1c4f1a72a3730
Emil Engler <me@emilengler.com>
2019-11-23 01:59:23 +0100

    Fix bug in cat display

--- categories.php
+++ categories.php
@@ -2,6 +2,7 @@
 require "core/include.php";
 $categories = simplexml_load_file("categories.xml");
 $episodes = getEpisodes($_GET["cat"]);
+$episode_chunk = $episodes;
 
 // Backwards comp
 $link = str_replace("?", "", $config["link"]);
