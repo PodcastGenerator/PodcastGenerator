54aa86a65a4f6c8d17adbf76ce7ab94fb19d4fbc
Emil Engler <me@emilengler.com>
2019-12-02 20:06:41 +0100

    Cleanup admin include

--- PodcastGenerator/core/include_admin.php
+++ PodcastGenerator/core/include_admin.php
@@ -6,7 +6,11 @@ if(!file_exists("../config.php")) {
 }
 include "misc/configsystem.php";
 include "misc/globs.php";
+include "episodes.php";
+include "feed_generator.php";
 include "buttons.php";
+include "freebox.php";
+include "backwards.php";
 // Until Podcast Generator 3.0 passwords were stored in MD5, which is inseucre since 2005
 // This file is wizard to convert old password to a more secure algorithim
 $config = getConfig("../config.php");
@@ -15,7 +19,11 @@ if(strlen($config["userpassword"]) == 32) {
     header("Location: ../core/misc/passwordconverter.php");
     die();
 }
-include "feed_generator.php";
-include "freebox.php";
-include "backwards.php";
+// Check if theme is compatible
+if (!file_exists($config["theme_path"] . "theme.json")) {
+    // Fallback to defalt theme if incompatible
+    updateConfig("../config.php", "theme_path", "themes/default/");
+}
+
+// Do backwards comp
 backwards_2_7_to_3_0($config["absoluteurl"]);
