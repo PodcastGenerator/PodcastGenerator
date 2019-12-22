0c15d99574b21750483602fb5d897939d91838aa
Emil Engler <me@emilengler.com>
2019-12-22 20:23:17 +0100

    Remove no translation warning

--- PodcastGenerator/setup/index.php
+++ PodcastGenerator/setup/index.php
@@ -14,7 +14,6 @@ require "securitycheck.php";
             <p>
                 Howdy and welcome to Podcast Generator <?php echo $version; ?>!<br>
                 Thanks for choosing a <a href="http://emilengler.com" target="_blank">Emil Engler</a> and <a href="http://betella.net" target="_blank">Alberto Betella</a> software, have a cookie 🍪!<br>
-                Unfortunaly the rewrite does not include a translation system yet, it will be added later on.<br>
                 This is Free and Open Source Software<br>
                 <br>
                 <hr>
