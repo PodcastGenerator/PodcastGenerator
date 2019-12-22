b2c87ec0e46d5e8aaf19948ea3b5596a531d7caf
Emil Engler <me@emilengler.com>
2019-12-22 05:11:35 +0100

    Fixes in include

--- PodcastGenerator/core/include_admin.php
+++ PodcastGenerator/core/include_admin.php
@@ -10,11 +10,13 @@ include "episodes.php";
 include "feed_generator.php";
 include "buttons.php";
 include "freebox.php";
-include "backwards.php";
-include "translation.php";
 // Until Podcast Generator 3.0 passwords were stored in MD5, which is inseucre since 2005
 // This file is wizard to convert old password to a more secure algorithim
 $config = getConfig("../config.php");
+// Backwards compatability
+include "backwards.php";
+// Load translations
+include "translation.php";
 // Check if the hash is MD5
 if(strlen($config["userpassword"]) == 32) {
     header("Location: ../core/misc/passwordconverter.php");
