b9e1a0af3f605fe701ae0d28e3ef671513650734
Emil Engler <me@emilengler.com>
2020-02-04 10:40:47 +0100

    Redirect to absolute hompage instead of admin index

--- PodcastGenerator/admin/episodes_edit.php
+++ PodcastGenerator/admin/episodes_edit.php
@@ -23,7 +23,7 @@ if (isset($_GET['delete'])) {
     unlink('../' . $config['upload_dir'] . $_GET['name']);
     // Delete the XML file
     unlink('../' . $config['upload_dir'] . pathinfo('../' . $config['upload_dir'] . $_GET['name'], PATHINFO_FILENAME) . '.xml');
-    header('Location: index.php');
+    header('Location: '.$config['url'].$config['indexfile']);
     die();
 }
 
