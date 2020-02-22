fb0a689607a5afddeb37e95fc269716c051275e1
Emil Engler <me@emilengler.com>
2020-02-22 16:37:29 +0100

    theme default: Open Admin Page in new tab

--- PodcastGenerator/themes/default/navbar.php
+++ PodcastGenerator/themes/default/navbar.php
@@ -18,7 +18,7 @@
             }
             ?>
             <li class="nav-item">
-                <a class="nav-link" href="admin/">Admin</a>
+                <a class="nav-link" href="admin/" target="_blank">Admin</a>
             </li>
         </ul>
     </div>
