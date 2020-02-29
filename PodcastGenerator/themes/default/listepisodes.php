1b4cfa75569dbaee1e11603b5fa5be1a2ecbfdba
Emil Engler <me@emilengler.com>
2020-02-29 11:19:40 +0100

    theme: Remove htmlspecialchars() in default theme
    Fixes #210

--- PodcastGenerator/themes/default/listepisodes.php
+++ PodcastGenerator/themes/default/listepisodes.php
@@ -11,33 +11,33 @@ for ($i = 0; $i < sizeof($episode_chunk); $i++) {
         $type = 'video';
     } elseif (substr($mime, 0, 5) == 'audio' || $mime == 'application/ogg') {
         $type = 'audio';
-        $metadata = '(' . htmlspecialchars($item[$i]["episode"]["fileInfoPG"]["bitrate"]) . ' kbps ' . htmlspecialchars($item[$i]["episode"]["fileInfoPG"]["frequency"]) . ' Hz)';
+        $metadata = '(' . $item[$i]["episode"]["fileInfoPG"]["bitrate"] . ' kbps ' . $item[$i]["episode"]["fileInfoPG"]["frequency"] . ' Hz)';
     } else {
         $type = 'invalid';
     }
 
     echo '<div class="col-lg-6">';
-    echo '  <h1><a href="' . $config['indexfile'] . '?' . $link . '=' . $item[$i]["episode"]["filename"] . '">' . htmlspecialchars($item[$i]["episode"]["titlePG"]) . '</a></h1>';             // Headline
-    echo '  <small>' . htmlspecialchars($item[$i]["episode"]["moddate"]) . '</small><br>';                                                                    // Pub Date
-    echo '  <small>' . htmlspecialchars($item[$i]["episode"]["shortdescPG"]) . '</small><br>';                                                                // Short description
+    echo '  <h1><a href="' . $config['indexfile'] . '?' . $link . '=' . $item[$i]["episode"]["filename"] . '">' . $item[$i]["episode"]["titlePG"] . '</a></h1>';
+    echo '  <small>' . $item[$i]["episode"]["moddate"] . '</small><br>';
+    echo '  <small>' . $item[$i]["episode"]["shortdescPG"] . '</small><br>';
     // Display edit button if admin is logged in
     if (isset($_SESSION["username"])) {
         echo '  <a class="btn btn-dark btn-sm" href="admin/episodes_edit.php?name=' . $item[$i]["episode"]["filename"] . '">' . $editdelete . '</a>';
     }
     echo '  <a class="btn btn-outline-primary btn-sm" href="' . $config['indexfile'] . '?' . $link . '=' . $item[$i]["episode"]["filename"] . '">' . $more . '</a>
-                <a class="btn btn-outline-success btn-sm" href="media/' . $item[$i]["episode"]["filename"] . '">' . $download . '</a><br>';                      // Buttons
+                <a class="btn btn-outline-success btn-sm" href="media/' . $item[$i]["episode"]["filename"] . '">' . $download . '</a><br>';
     if ($type != 'invalid') {
         echo '  <small>' . $filetype . ': ' . strtoupper(pathinfo($config["upload_dir"] . $item[$i]["episode"]["filename"], PATHINFO_EXTENSION)) . '
-                - ' . $size . ': ' . htmlspecialchars($item[$i]["episode"]["fileInfoPG"]["size"]) . ' MB - ' . $duration . ': ' . htmlspecialchars($item[$i]["episode"]["fileInfoPG"]["duration"]) . 'm ' . $metadata . '</small><br>';
+                - ' . $size . ': ' . $item[$i]["episode"]["fileInfoPG"]["size"] . ' MB - ' . $duration . ': ' . $item[$i]["episode"]["fileInfoPG"]["duration"] . 'm ' . $metadata . '</small><br>';
     }
     if (strtolower($config["enablestreaming"]) == "yes") {
         if ($type == 'audio') {
             echo '  <audio controls>';
-            echo '      <source src="' . htmlspecialchars($config["upload_dir"]) . htmlspecialchars($item[$i]["episode"]["filename"]) . '" type="' . $mime . '">';
+            echo '      <source src="' . $config["upload_dir"] . $item[$i]["episode"]["filename"] . '" type="' . $mime . '">';
             echo '  </audio>';
         } elseif ($type == 'video') {
             echo '  <video controls width="250">';
-            echo '      <source src="' . htmlspecialchars($config["upload_dir"]) . htmlspecialchars($item[$i]["episode"]["filename"]) . '" type="' . $mime . '">';
+            echo '      <source src="' . $config["upload_dir"] . $item[$i]["episode"]["filename"] . '" type="' . $mime . '">';
             echo '  </video>';
         }
     }
--- PodcastGenerator/themes/default/singleepisode.php
+++ PodcastGenerator/themes/default/singleepisode.php
@@ -22,31 +22,31 @@ if (substr($mime, 0, 5) == 'video') {
     $type = 'video';
 } elseif (substr($mime, 0, 5) == 'audio' || $mime == 'application/ogg') {
     $type = 'audio';
-    $metadata = '(' . htmlspecialchars($correctepisode["episode"]["fileInfoPG"]["bitrate"]) . ' kbps ' . htmlspecialchars($correctepisode["episode"]["fileInfoPG"]["frequency"]) . ' Hz)';
+    $metadata = '(' . $correctepisode["episode"]["fileInfoPG"]["bitrate"] . ' kbps ' . $correctepisode["episode"]["fileInfoPG"]["frequency"] . ' Hz)';
 } else {
     $type = 'invalid';
 }
 
 echo '<div class="col-lg-12">';
-echo '  <h1>' . htmlspecialchars($correctepisode["episode"]["titlePG"]) . '</h1>';                                                                              // Headline
-echo '  <small>' . htmlspecialchars($correctepisode["episode"]["moddate"]) . '</small><br>';                                                                    // Pub Date
-echo '  <small>' . htmlspecialchars($correctepisode["episode"]["shortdescPG"]) . '</small><br>';                                                                // Short description
+echo '  <h1>' . $correctepisode["episode"]["titlePG"] . '</h1>';
+echo '  <small>' . $correctepisode["episode"]["moddate"] . '</small><br>';
+echo '  <small>' . $correctepisode["episode"]["shortdescPG"] . '</small><br>';
 if (isset($_SESSION["username"])) {
     echo '  <a class="btn btn-dark btn-sm" href="admin/episodes_edit.php?name=' . $episodes[$i]["episode"]["filename"] . '">' . $editdelete . '</a>';
 }
-echo '  <a class="btn btn-outline-success btn-sm" href="media/' . $correctepisode["episode"]["filename"] . '">' . $download . '</a><br>';              // Buttons
+echo '  <a class="btn btn-outline-success btn-sm" href="media/' . $correctepisode["episode"]["filename"] . '">' . $download . '</a><br>';
 if ($type != 'invalid') {
-    echo '  <small>' . $filetype . ': ' . htmlspecialchars(strtoupper(pathinfo($config["upload_dir"] . $correctepisode["episode"]["filename"], PATHINFO_EXTENSION))) . '
-                - ' . $size . ': ' . htmlspecialchars($correctepisode["episode"]["fileInfoPG"]["size"]) . ' MB - ' . $duration . ': ' . htmlspecialchars($correctepisode["episode"]["fileInfoPG"]["duration"]) . 'm ' . $metadata . '</small><br>';
+    echo '  <small>' . $filetype . ': ' . strtoupper(pathinfo($config["upload_dir"] . $correctepisode["episode"]["filename"], PATHINFO_EXTENSION)) . '
+                - ' . $size . ': ' . $correctepisode["episode"]["fileInfoPG"]["size"] . ' MB - ' . $duration . ': ' . $correctepisode["episode"]["fileInfoPG"]["duration"] . 'm ' . $metadata . '</small><br>';
 }
 if (strtolower($config["enablestreaming"]) == "yes") {
     if ($type == 'audio') {
         echo '  <audio controls>';
-        echo '      <source src="' . htmlspecialchars($config["upload_dir"]) . htmlspecialchars($episodes[$i]["episode"]["filename"]) . '" type="' . $mime . '">';
+        echo '      <source src="' . $config["upload_dir"] . $episodes[$i]["episode"]["filename"] . '" type="' . $mime . '">';
         echo '  </audio>';
     } elseif ($type == 'video') {
         echo '  <video controls width="250">';
-        echo '      <source src="' . htmlspecialchars($config["upload_dir"]) . htmlspecialchars($correctepisode["episode"]["filename"]) . '" type="' . $mime . '">';
+        echo '      <source src="' . $config["upload_dir"] . $correctepisode["episode"]["filename"] . '" type="' . $mime . '">';
         echo '  </video>';
     }
 }
