ef19ebc2bcf79f5cd5906270993e0f7b965d5dc7
Emil Engler <me@emilengler.com>
2020-03-01 12:43:22 +0100

    script: Add comment to setupEnv.sh

--- contrib/devtools/setupEnv.sh
+++ contrib/devtools/setupEnv.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # This script is for developer use
 # Sets folder permissions so that the webserver can write on it
+# Made for Debian 10, adjust user groups if neccesary
 chmod 775 PodcastGenerator
 chmod 775 PodcastGenerator/media
 chmod 775 PodcastGenerator/images
