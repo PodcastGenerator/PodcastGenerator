a774ebc273fd025cf4868bc1141b5520e7a7fd54
Emil Engler <me@emilengler.com>
2020-04-26 09:49:39 +0200

    config: Parse empty strings

--- PodcastGenerator/core/misc/configsystem.php
+++ PodcastGenerator/core/misc/configsystem.php
@@ -76,7 +76,7 @@ function getConfig($path = 'config.php')
         if ($lines[$i][0] == "\t")
             $lines[$i] = substr($lines[$i], 1);
 
-        preg_match('/\$(.+?) = ["\'](.+?)["\'];/', $lines[$i], $strout);    // Get all strings
+        preg_match('/\$(.+?) = ["\'](.+?)?["\'];/', $lines[$i], $strout);    // Get all strings
         preg_match('/\$(.+?) = ([^"\']+);/', $lines[$i], $nonstr); // Get all non strings
         if (sizeof($nonstr) == 3) {
             // Cut of escape chars if there are any
@@ -92,9 +92,14 @@ function getConfig($path = 'config.php')
             if ($strout[2] != '"') {
                 $strout[2] = str_replace("\\", '', $strout[2]);
                 $configmap[$strout[1]] = $strout[2];
+            // Make the string empty on errors
             } else {
                 $configmap[$strout[1]] = '';
             }
+        }
+        // If the string is empty
+        elseif (sizeof($strout) == 2) {
+            $configmap[$strout[1]] = '';
         }
         else {
             continue;
