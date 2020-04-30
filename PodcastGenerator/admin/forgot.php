6bd8c3857a6443603eef69e4e57f55e07b0bd3a1
Emil Engler <me@emilengler.com>
2020-04-30 12:47:48 +0200

    doc: Fix typo

--- PodcastGenerator/admin/forgot.php
+++ PodcastGenerator/admin/forgot.php
@@ -13,7 +13,7 @@
         <h3><?php echo _('No password, no problem!'); ?></h3>
         <p>
         <?php
-        echo _('We offer a tool which allows you to easily recover your password. However, the danger of this is that as long as this file is only, anyone can reset this password so this file shouldn\'t be online that long.');
+        echo _('We offer a tool which allows you to easily recover your password. However, the danger of this is that as long as this file is online, anyone can reset this password so this file shouldn\'t be online that long.');
         echo '<br>';
         echo sprintf(_('In the ZIP file of this version which you downloaded there is a file in the %s folder called %s. Upload this file to into the admin directory with the name %s'), "contrib/recover", "reset.php", "reset.php");
         echo _('If you no don\'t have this ZIP file anymore, don\'t worry! All versions of Podcast Generator ever released since 2.5 are available on the Podcast Generator Homepage.');
