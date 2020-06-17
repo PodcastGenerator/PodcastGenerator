2f8ea0fae1306011733a22e49be61123636646b1
NeaMitika <59248866+NeaMitika@users.noreply.github.com>
2020-06-17 23:32:51 +0200

    Updated the navbar with the requests of Emil
    Made the navbar background blue (red was too much, but i agree that it should be different from the main navbar, as an alternative green works well too)
    Reversed the changes to "Home" button and  "View Podcast" button
    
    Since i made the navbar nested inside a container class i strugle with the space and in my case the last 2 buttons in the navbar are on 2 rows.
    
    I will try to find a solution before mergin this pull

--- PodcastGenerator/admin/navbar.php
+++ PodcastGenerator/admin/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
 <div class="container"> 
     <a class="navbar-brand" href="index.php">Admin</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="<?php echo _('Toggle navigation'); ?>">
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="../index.php">Home</a>
+                <a class="nav-link" href="index.php">Home</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -59,9 +59,12 @@
                 </div>
             </li>
         </ul>
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item nav-pills">
-                <a class="nav-link active p-1" href="logout.php">Logout - <?php echo $_SESSION["username"]; ?></a>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo $config['url']; ?>" target="_blank"><?php echo _('View Podcast'); ?></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="logout.php">Logout - <?php echo $_SESSION["username"]; ?></a>
             </li>
         </ul>
     </div>
