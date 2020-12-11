6700376b5f508b6cf6049122562e8e6038131db0
Chris Charabaruk <chris.charabaruk@outlook.com>
2020-12-10 21:24:40 -0500

    implement WebSub ping function
    This adds a function to send a WebSub publish message to the configured
    server, and adds that function to the pingServices() function so it will
    be called whenever the podcast updates.

--- PodcastGenerator/core/pinger.php
+++ PodcastGenerator/core/pinger.php
@@ -7,6 +7,39 @@
 #
 # This is Free Software released under the GNU/GPL License.
 ############################################################
+function pingWebSub()
+{
+    // Get the global config
+    global $config;
+    // Exit early if no WebSub service isn't set up
+    if (!$config['websub_server']) { return; }
+    // Ping it
+    $data = array(
+        'hub.mode' => 'publish',
+        'hub.url' => $config['url'] . $config['feed_dir'] . 'feed.xml'
+    );
+    $opts = array(
+        CURLOPT_URL => $config['websub_server'],
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => $data,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded')
+    );
+    try {
+        $handle = curl_init();
+        curl_setopt_array($handle, $opts);
+        $result = curl_exec($handle);
+    }
+    catch (Exception $e) {
+        // write a warning to stderr, but don't blow anything up
+        error_log($e, 0);
+    }
+    finally {
+        if ($handle) curl_close($handle);
+    }
+}
+
 function pingServices()
 {
+    pingWebSub();
 }
