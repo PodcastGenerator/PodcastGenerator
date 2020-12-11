b8ddeae338521eb0fff12d9f4d0e31ed3c500258
Chris Charabaruk <chris.charabaruk@outlook.com>
2020-12-10 20:57:55 -0500

    add pinger infrastructure
    Provide a basic pingServices() function whose job will be to call the
    functions which actually ping each individual service.

---
+++ PodcastGenerator/core/pinger.php
@@ -0,0 +1,12 @@
+<?php
+############################################################
+# PODCAST GENERATOR
+#
+# Created by Alberto Betella and Emil Engler
+# http://www.podcastgenerator.net
+#
+# This is Free Software released under the GNU/GPL License.
+############################################################
+function pingServices()
+{
+}
