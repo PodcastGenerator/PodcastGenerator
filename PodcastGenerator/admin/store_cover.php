7a5b03089e1d0d5d0e405551879956c9ce34a73c
Gorom <1423386+Go-rom@users.noreply.github.com>
2021-01-23 18:11:24 +0100

    Cover is now stored with a new variable
    This commit aims to support the new 'podcast_cover' variable.
    When a user uploads a new podcat cover, the file is stored in the images folder with its original filename, it doesn't overwrite itunes_image.jpg anymore.
    Then the filename is stored in config.php within the new variable : 'podcast_cover' 
    Also this page now displays the cover stored in config.php

--- PodcastGenerator/admin/store_cover.php
+++ PodcastGenerator/admin/store_cover.php
@@ -25,12 +25,14 @@ if (isset($_GET['upload'])) {
     }
 
     // Now everything is cool and the file can uploaded
-    if (!move_uploaded_file($_FILES['file']['tmp_name'], $config['absoluteurl'] . $config['img_dir'] . 'itunes_image.jpg')) {
+    if (!move_uploaded_file($_FILES['file']['tmp_name'], $config['absoluteurl'] . $config['img_dir'] . $_FILES['file']['name'])) {
         $error = _('File was not uploaded');
         goto error;
     } else {
         // Wait a few seconds so the upload can finish
         sleep(3);
+        updateConfig('../config.php', 'podcast_cover', $_FILES['file']['name']);
+	generateRSS();
         header('Location: store_cover.php');
         die();
     }
@@ -63,7 +65,7 @@ if (isset($_GET['upload'])) {
         }
         ?>
         <h3><?php echo _('Current Cover'); ?></h3>
-        <img src="<?php echo $config['url'] . $config['img_dir']; ?>itunes_image.jpg" style="max-height: 350px; max-width: 350px;">
+        <img src="<?php echo $config['url'] . $config['img_dir'] . $config['podcast_cover']; ?>" style="max-height: 350px; max-width: 350px;">
         <hr>
         <h3><?php echo _('Upload new cover'); ?></h3>
         <form action="store_cover.php?upload=1" method="POST" enctype="multipart/form-data">
