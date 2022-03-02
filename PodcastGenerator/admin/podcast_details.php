af6d307c3891eb2b9cefcbe8524ea93709e6f6fc
Chris Charabaruk <chris.charabaruk@outlook.com>
2022-03-01 21:44:15 -0500

    Replace hard-coded feed language list
    Use podcast_languages.xml to build the list of selectable feed languages
    so that we can now support country or region variants.
    
    Closes #450

--- PodcastGenerator/admin/podcast_details.php
+++ PodcastGenerator/admin/podcast_details.php
@@ -10,6 +10,8 @@
 require 'checkLogin.php';
 require '../core/include_admin.php';
 
+$languages = simplexml_load_file('../components/supported_languages/podcast_languages.xml');
+
 if (isset($_GET['edit'])) {
     checkToken();
     foreach ($_POST as $key => $value) {
@@ -81,79 +83,9 @@ $custom_tags = getCustomFeedTags();
             <input type="text" name="author_email" value="<?= htmlspecialchars($config['author_email']) ?>" class="txt"><br>
             <?= _('Feed Language'); ?>: (<?= _('Main language of your podcast') ?>)<br>
             <select name="feed_language">
-                <option value="af">Afrikanns</option>
-                <option value="sq">Albanian</option>
-                <option value="ar">Arabic</option>
-                <option value="hy">Armenian</option>
-                <option value="eu">Basque</option>
-                <option value="bn">Bengali</option>
-                <option value="bg">Bulgarian</option>
-                <option value="ca">Catalan</option>
-                <option value="km">Cambodian</option>
-                <option value="zh">Chinese (Mandarin)</option>
-                <option value="hr">Croation</option>
-                <option value="cs">Czech</option>
-                <option value="da">Danish</option>
-                <option value="nl">Dutch</option>
-                <option value="en" selected>English</option>
-                <option value="eo">Esperanto</option>
-                <option value="et">Estonian</option>
-                <option value="fj">Fiji</option>
-                <option value="fi">Finnish</option>
-                <option value="fr">French</option>
-                <option value="ka">Georgian</option>
-                <option value="de">German</option>
-                <option value="el">Greek</option>
-                <option value="gu">Gujarati</option>
-                <option value="he">Hebrew</option>
-                <option value="hi">Hindi</option>
-                <option value="hu">Hungarian</option>
-                <option value="is">Icelandic</option>
-                <option value="id">Indonesian</option>
-                <option value="ga">Irish</option>
-                <option value="it">Italian</option>
-                <option value="ja">Japanese</option>
-                <option value="jw">Javanese</option>
-                <option value="ko">Korean</option>
-                <option value="la">Latin</option>
-                <option value="lv">Latvian</option>
-                <option value="lt">Lithuanian</option>
-                <option value="mk">Macedonian</option>
-                <option value="ms">Malay</option>
-                <option value="ml">Malayalam</option>
-                <option value="mt">Maltese</option>
-                <option value="mi">Maori</option>
-                <option value="mr">Marathi</option>
-                <option value="mn">Mongolian</option>
-                <option value="ne">Nepali</option>
-                <option value="no">Norwegian</option>
-                <option value="fa">Persian</option>
-                <option value="pl">Polish</option>
-                <option value="pt">Portuguese</option>
-                <option value="pa">Punjabi</option>
-                <option value="qu">Quechua</option>
-                <option value="ro">Romanian</option>
-                <option value="ru">Russian</option>
-                <option value="sm">Samoan</option>
-                <option value="sr">Serbian</option>
-                <option value="sk">Slovak</option>
-                <option value="sl">Slovenian</option>
-                <option value="es">Spanish</option>
-                <option value="sw">Swahili</option>
-                <option value="sv">Swedish </option>
-                <option value="ta">Tamil</option>
-                <option value="tt">Tatar</option>
-                <option value="te">Telugu</option>
-                <option value="th">Thai</option>
-                <option value="bo">Tibetan</option>
-                <option value="to">Tonga</option>
-                <option value="tr">Turkish</option>
-                <option value="uk">Ukranian</option>
-                <option value="ur">Urdu</option>
-                <option value="uz">Uzbek</option>
-                <option value="vi">Vietnamese</option>
-                <option value="cy">Welsh</option>
-                <option value="xh">Xhosa</option>
+                <?php foreach ($languages as $lang) { ?>
+                    <option value="<?= htmlspecialchars($lang->code) ?>"<?= $config['feed_language'] == $lang->code ? " selected" : "" ?>><?= htmlspecialchars($lang->name) ?></option>
+                <?php } ?>
             </select><br>
             <?= _('Explicit Podcast') ?>:<br>
             <input type="radio" name="explicit_podcast" value="yes" <?= $config['explicit_podcast'] == 'yes' ? 'checked' : '' ?>> <?= _('Yes'); ?> <input type="radio" name="explicit_podcast" value="no" <?= $config['explicit_podcast'] == 'no' ? 'checked' : '' ?>> <?= _('No') ?><br>
