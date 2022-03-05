ebce1f7d37f7812fce67e4ee2fec7bef14daf722
Chris Charabaruk <chris.charabaruk@outlook.com>
2022-03-05 14:50:40 -0500

    refactor: clean up typos
    Also add additional vertical whitespace to enhance readability.

--- PodcastGenerator/core/include.php
+++ PodcastGenerator/core/include.php
@@ -8,23 +8,26 @@
 #
 # This is Free Software released under the GNU/GPL License.
 ############################################################
+
 include 'misc/security.php';
 include 'misc/configsystem.php';
 include 'misc/globs.php';
+
 $config = getConfig('config.php');
 if ($config['podcastgen_version'] != $version) {
-    // Backwards compatibity
+    // Backwards compatibility
     include 'backwards.php';
     backwards_3_1_to_3_2($config['absoluteurl']);
     die("Please refresh");
 }
+
 include 'episodes.php';
 include 'feed_generator.php';
 include 'pinger.php';
 include 'buttons.php';
 include 'freebox.php';
-// Until Podcast Generator 3.0 passwords were stored in MD5, which is inseucre since 2005
-// This file is wizard to convert old password to a more secure algorithim
+// Until Podcast Generator 3.0 passwords were stored in MD5, which is insecure since 2005
+// This file is wizard to convert old password to a more secure algorithm
 // Load useful functions
 include 'misc/functions.php';
 // Load translation
