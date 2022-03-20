80d1a9550dfee695cf2c6864e0a68cd7def91df4
Chris Charabaruk <chris.charabaruk@outlook.com>
2022-03-20 12:49:02 -0400

    Update generateTranslation.sh
    Match it to how I've been running xgettext manually.

--- contrib/devtools/generateTranslation.sh
+++ contrib/devtools/generateTranslation.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 # Execute this file from the root of the git repository
 # This file generates the translation template and the english base
-# ./contrib/generateTranslation.sh <VERSIONTAG>
-xgettext --copyright-holder="The Podcast Generator Translation Team" --no-wrap --package-name="Podcast Generator" --package-version="$1" --language=PHP --from-code=UTF-8 -o PodcastGenerator/components/locale/messages.pot PodcastGenerator/admin/*.php PodcastGenerator/core/*.php PodcastGenerator/*.php
+# ./contrib/devtools/generateTranslation.sh <VERSIONTAG>
+xgettext \
+    --copyright-holder="The Podcast Generator Translation Team" \
+    --package-name="Podcast Generator" \
+    --package-version="$1" \
+    --msgid-bugs-address="https://github.com/PodcastGenerator/PodcastGenerator/issues/new" \
+    --language=PHP \
+    --add-comments="#" \
+    --from-code=UTF-8 \
+    -o PodcastGenerator/components/locale/messages.pot \
+    PodcastGenerator/*.php \
+    PodcastGenerator/admin/*.php \
+    PodcastGenerator/core/*.php \
+    PodcastGenerator/core/misc/*.php \
+    PodcastGenerator/setup/*.php \
+    PodcastGenerator/themes/default/*.php \
