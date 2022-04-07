8b63245e97ca8f88998e3c9cbd1b0afab70a8a13
Chris Charabaruk <github@chris.charabaruk.com>
2022-04-07 19:30:05 -0400

    fix(feed_generator.php): swap summary tag content
    Put the short description in the `<itunes:summary>` tag instead of the
    long description, and put the long description, when present, into the
    `<description>` tag instead.
    
    This may lead to some confusion for listeners to existing shows which
    upgrade from an older version of Podcast Generator, at it will appear
    that the short and long descriptions have changed places in their
    podcast apps. Blame the lack of any real specification around summaries
    and full descriptions/show notes that all apps adhere to.
    
    fixes #522

--- PodcastGenerator/core/feed_generator.php
+++ PodcastGenerator/core/feed_generator.php
@@ -145,10 +145,12 @@ function generateRSS()
         <item>' . "\n";
         $item .= $indent . '<title>' . $file->episode->titlePG . '</title>' . $linebreak;
         $item .= $indent . '<itunes:subtitle>' . $file->episode->shortdescPG . '</itunes:subtitle>' . $linebreak;
-        $item .= $indent . '<description>' . $file->episode->shortdescPG . '</description>' . $linebreak;
         if (!empty(trim($file->episode->longdescPG))) {
-            $item .= $indent . '<itunes:summary><![CDATA[' . $file->episode->longdescPG . ']]></itunes:summary>' . $linebreak;
+            $item .= $indent . '<description><![CDATA[' . $file->episode->longdescPG . ']]></description>' . $linebreak;
+        } else {
+            $item .= $indent . '<description>' . $file->episode->shortdescPG . '</description>' . $linebreak;
         }
+        $item .= $indent . '<itunes:summary>' . $file->episode->shortdescPG . '</itunes:summary>' . $linebreak;
         $item .= $indent . '<link>' . $config['url'] . '?' . $link . '=' . $files[$i]['filename'] . '</link>' . $linebreak;
         $item .= $indent . '<enclosure url="' . $original_full_filepath . '" length="' . filesize($config['absoluteurl'] . $config['upload_dir'] . $files[$i]['filename']) . '" type="' . $mimetype . '"></enclosure>' . $linebreak;
         $item .= $indent . '<guid>' . $config['url'] . "?" . $link . "=" . $files[$i]['filename'] . '</guid>' . $linebreak;
