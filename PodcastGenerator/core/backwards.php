c946e9fb59c39d4252046bb168c1b821b8d30e2a
Chris Charabaruk <github@chris.charabaruk.com>
2022-04-20 07:12:17 -0400

    chore: begin work on 3.2.4

--- PodcastGenerator/core/backwards.php
+++ PodcastGenerator/core/backwards.php
@@ -8,7 +8,7 @@
 #
 # This is Free Software released under the GNU/GPL License.
 ############################################################
-function backwards_3_1_to_3_2_3($absoluteurl)
+function backwards_3_1_to_3_2_4($absoluteurl)
 {
     session_start();
     session_destroy();
@@ -22,6 +22,7 @@ function backwards_3_1_to_3_2_3($absoluteurl)
             || $config['podcastgen_version'] == '3.2'
             || $config['podcastgen_version'] == '3.2.1'
             || $config['podcastgen_version'] == '3.2.2'
+            || $config['podcastgen_version'] == '3.2.3'
         )
     ) {
         return;
