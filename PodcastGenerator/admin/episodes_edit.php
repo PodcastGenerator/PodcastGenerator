e3e6c7e4d1eec2e90a68b627a95ea7f261e6ce27
Chris Charabaruk <github@chris.charabaruk.com>
2022-05-24 16:41:37 -0400

    backport: Let authorname and authoremail be optional in episodes_edit.php (#591)
    Backport of a5b29ec25146dc8727a3130eb43dec535e320a60

--- PodcastGenerator/admin/episodes_edit.php
+++ PodcastGenerator/admin/episodes_edit.php
@@ -47,9 +47,7 @@ if (count($_POST) > 0) {
         $_POST['shortdesc'],
         $_POST['date'],
         $_POST['time'],
-        $_POST['explicit'],
-        $_POST['authorname'],
-        $_POST['authoremail']
+        $_POST['explicit']
     ];
     // Check if fields are missing
     for ($i = 0; $i < count($req_fields); $i++) {
@@ -71,7 +69,7 @@ if (count($_POST) > 0) {
     }
 
     // Check author e-mail
-    if (isset($_POST['authoremail'])) {
+    if (!empty($_POST['authoremail'])) {
         if (!filter_var($_POST['authoremail'], FILTER_VALIDATE_EMAIL)) {
             $error = _('Invalid Author E-Mail provided');
             goto error;
@@ -433,7 +431,7 @@ $selected_cats = array(
                         <br>
                     </div>
                     <div class="form-group">
-                        <label for="authorname" class="req"><?= _('Author') ?>:</label><br>
+                        <label for="authorname"><?= _('Author') ?>:</label><br>
                         <input type="text" id="authorname" name="authorname" class="form-control"
                                placeholder="Author Name"
                                value="<?= htmlspecialchars($episode->episode->authorPG->namePG) ?>">
