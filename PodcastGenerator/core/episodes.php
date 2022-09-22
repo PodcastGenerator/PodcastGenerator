PodcastGenerator/PodcastGenerator

commit fc058e0a2c45da2b56538b7cd6c9b2e0f8832590

fix: trim nasty characters from filenames
Fix up makeEpisodeFilename() and makeUniqueFilename() so that characters
outside the alphanumeric range that aren't the period, dash, or
underscore are replaced with an underscore.

refs #691

PodcastGenerator/core/episodes.php
@@ -409,7 +409,7 @@ function compare_mtimes($a, $b)
  */
 function makeEpisodeFilename($directory, $date, $filename)
 {
-    $filename = strtolower(str_replace(' ', '_', $filename));
+    $filename = strtolower(trim(preg_replace('/[^a-zA-Z0-9._-]+/', '_', $title), '_'));
     $targetfile = $directory . $date . '_' . $filename;
 
     if (file_exists($targetfile)) {

PodcastGenerator/core/misc/functions.php
@@ -99,7 +99,8 @@ function makeUniqueFilename($path)
     // make sure that we have a real directory path with filename attached
     // just putting $path into realpath() will fail if the file doesn't exist
     $pathinfo = pathinfo($path);
-    $realpath = realpath($pathinfo['dirname']) . '/' . $pathinfo['basename'];
+    $filename = strtolower(trim(preg_replace('/[^a-zA-Z0-9_-]+/', '_', $pathinfo['filename']), '_'));
+    $realpath = realpath($pathinfo['dirname']) . '/' . $filename . $pathinfo['extension'];
 
     // if the existing path doesn't exist, we're unique!
     if (!file_exists($realpath)) {

commit 3e46ad37d1ee03ca8035dddc70fd6d194ef60057

fix(episodes): correct variable name
Fix a bug introduced in commit fc058e0a.

PodcastGenerator/core/episodes.php
@@ -404,7 +404,7 @@ function compare_mtimes($a, $b)
  */
 function makeEpisodeFilename($directory, $date, $filename)
 {
-    $filename = strtolower(trim(preg_replace('/[^a-zA-Z0-9._-]+/', '_', $title), '_'));
+    $filename = strtolower(trim(preg_replace('/[^a-zA-Z0-9._-]+/', '_', $filename), '_'));
     $targetfile = $directory . $date . '_' . $filename;
 
     if (file_exists($targetfile)) {